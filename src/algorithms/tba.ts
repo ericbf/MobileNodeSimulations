6c9d78412fdf34e6fffaf9558af1a37fff143f59
ADAM NICHOLS <ANichols4@protonmail.com>
2019-12-04 10:57:31 -0500

    Added tba.ts file, moved some code from hba.ts to it

---
+++ src/algorithms/tba.ts
@@ -0,0 +1,32 @@
+import { Node } from "../models/node"
+import { Hole } from "../models/hole"
+import {
+	distanceBetween,
+	stringify,
+	withTransposed,
+	transposed,
+	omit,
+	debug
+} from "../helpers"
+import { verbosity } from ".."
+
+export function tba(nodes: Node[], holes: Hole[]): Node[] {
+	/**
+	 * The coefficient matrix, `matrix[i][j]`. The `i`s are node index, and the `j`s are the hole index.
+	 */
+	let matrix = createDistanceMatrix(nodes, holes)
+}
+
+/** Create a square distance matrix with each column is the distance from a node to each hole. If the number of nodes isn't equal the number of holes, fill the array with `NaN` to make it square. */
+export function createDistanceMatrix(nodes: Node[], holes: Hole[]) {
+	const size = Math.max(nodes.length, holes.length)
+
+	return Array.from({ length: size }).map((_, i) =>
+		Array.from({ length: size }).map((_, j) => {
+			const node = nodes[i]
+			const hole = holes[j]
+
+			return !node || !hole ? NaN : distanceBetween(node, hole)
+		})
+	)
+}
